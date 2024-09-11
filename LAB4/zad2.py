4cdf8e2ec6ea7ee5dbf78b5ad9b790f930979fb7
szmpns <szymekkoziol@vp.pl>
2024-09-11 17:30:44 +0200

    LAB4

--- LAB4/zad2.py
+++ LAB4/zad2.py
@@ -1,22 +1,25 @@
 import numpy as np
+import psutil
+import os
 
 def print_memory_usage():
-    print("Memory Usage:")
-    print(np.array([1]))
+    process = psutil.Process(os.getpid())
+    memory_usage = process.memory_info().rss / (1024 * 1024)  # Zuzycie pamieci w MB
+    print(f"Memory Usage: {memory_usage:.2f} MB")
 
 def main():
     array_of_arrays = [np.zeros((1000, 1000), dtype=float)]
 
     print_memory_usage()
 
-    input("Naciśnij Enter, aby rozszerzyć tablicę...")
+    input("Press Enter to expand the array...")
 
     new_array = np.zeros((1000, 1000), dtype=float)
     array_of_arrays.append(new_array)
 
     print_memory_usage()
 
-    input("Naciśnij Enter, aby zakończyć program...")
+    input("Press Enter to exit the program...")
 
 if __name__ == "__main__":
     main()
